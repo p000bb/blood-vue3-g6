653e46c01e3a6588079eade9c6ea820c572a5542
p000bb <934371212@qq.com>
2024-08-29 11:18:31 +0800

    [add]:tailwind路径配置

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 export default {
   content: [
-    // "./index.html",
-    // "./src/**/*.{js,ts,jsx,tsx,vue}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
   ],
   theme: {
     extend: {},
